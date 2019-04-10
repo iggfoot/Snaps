214675ef82517effd16c256a291d1fb83164af53
iggfoot <ijneville19@gmail.com>
2019-04-10 03:45:09 -0400

    added button & event listener

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-var tester = prompt("Hit enter to get roasted!");
+
+function waitAMinute() {
+    //handles user prompt
+    var userChoice =
+};
+
+function roastMaker() {
+    /*Handles the roast generator
+      1.initiate user to pick a number between 1 and 5
+      2.generates that number of snaps
+      3.returns a snap at random*/
+};
+
+function handleButtonClick() {
+    /* 1.event listener for button
+       2.roast generator*/
+    $("button").click(function() {
+        console.log("handleButtonClick ran");
+    });
+}
+
+handleButtonClick();
