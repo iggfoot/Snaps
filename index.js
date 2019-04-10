090337dfc91cba59b16f4f70e8ad3adba39eb919
iggfoot <ijneville19@gmail.com>
2019-04-10 04:05:11 -0400

    working randomizer

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 
-function waitAMinute() {
-    //handles user prompt
-    var userChoice =
-};
+
 
 function roastMaker() {
     /*Handles the roast generator
       1.initiate user to pick a number between 1 and 5
       2.generates that number of snaps
       3.returns a snap at random*/
+      console.log("roastMaker method ran")
+      var possibleRoasts = Math.ceil(Math.random()*9);
+      console.log(possibleRoasts);
+
 };
 
 function handleButtonClick() {
     /* 1.event listener for button
        2.roast generator*/
     $("button").click(function() {
-        console.log("handleButtonClick ran");
+        console.log("handleButtonClick method ran");
+        roastMaker();
     });
 }
 
